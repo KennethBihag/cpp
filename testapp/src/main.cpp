d6a0e10a81e02acb15acdf74cfe95bdbae59909e
KennethBihag <1618kbb@gmail.com>
2024-01-28 00:57:20 +0800

    Test void* behavior in testapp

--- testapp/src/main.cpp
+++ testapp/src/main.cpp
@@ -5,6 +5,8 @@
 #include "common/include/common.h"
 #endif
 
+#include <malloc.h>
+#include <stdio.h>
 #include <iostream>
 using namespace std;
 
@@ -22,12 +24,61 @@ int main(int argc, const char *argv[])
 	mergesort_int(sample,sizeof(sample)/sizeof(int));
 	print_intarr_elems(sample,sizeof(sample)/sizeof(int));
 	#endif
+	
+	cout << "Sizes of pointers:\n";
+	printf("char:%lld, short:%lld, int:%lld, long:%lld, long long:%lld,"
+		"void:%lld\n",sizeof(char*),sizeof(short*),sizeof(int*),sizeof(long*),
+		sizeof(long long*),sizeof(void*));
 
-	{
-		TestApp ta;
+	char ch = 'x';
+	int in = 666;
+	long long li = 12345LL;
+	const char *streng = "Hello there!";
+
+	void *vps[10];
+	void *vpsall = malloc(10*sizeof(void*));
+	void **vpsptr = (void**)vpsall;
+	unsigned long long *p = (unsigned long long*)vpsall;
+	for(int i=1; i<=10; i++){
+		switch(i){
+		case 1:
+			vps[i-1] = &ch;
+			p[i-1] = ch;
+			break;
+		case 2:
+			vps[i-1] = &in;
+			p[i-1] = in;
+			break;
+		case 3:
+			vps[i-1] = &li;
+			p[i-1] = (unsigned long long)li;
+			break;
+		case 4:
+			vps[i-1] = (void*)streng;
+			p[i-1] = (unsigned long long)streng;
+			break;
+		default:
+			vps[i-1] = (void*)(i);
+			p[i-1] = i*2;
+		}
 	}
 
-	
+	printf("%X:1:%c\n",vpsall,*(char*)vpsall);
+	printf("%X:2:%d\n",vpsall+8,*(int*)(vpsall+8));
+	printf("%X:3:%llu\n",vpsall+16,*(unsigned long long*)(vpsall+16));
+	char **tch = (char **)(vpsall+24);
+	printf("%X:4:%s\n",vpsall+24,*tch);
+
+	cout << "TEST VOID**\n";
+	for(int i=0;i<10;++i){
+		printf("%d ADDR:%X\n",i+1,vpsptr+i);
+		printf("\tVALUE:%llu\n",(unsigned long long)vpsptr[i]);
+		printf("\t\tVALUE2:%llu\n",*(vpsptr+i));
+		if(i==3){
+			printf("\t\t\t%s\n",vpsptr[i]);
+		}
+	}
+
 	cout << "testapp ended" << endl;
 	return 0;
 }
