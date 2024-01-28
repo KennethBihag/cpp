9bbcbb3314e884ce1b6de6e2423c3da18cb126d6
KennethBihag <1618kbb@gmail.com>
2024-01-28 12:31:46 +0800

    Fix errors & warnings for threading Linux build

--- threading/src/threading.c
+++ threading/src/threading.c
@@ -9,7 +9,7 @@ extern char *g_srule;
 MyThread *mythreads[0xFF] = {0};
 
 #ifndef _WIN32
-static int thrd_create_dummy(thrd_t *ID, int (*FUNC)(void *), void *ARGS)
+static int thrd_create_dummy(thrd_t *ID, int (*FUNC)(void*), void *ARGS)
 {
     *ID = *ID % 256;
     int code = FUNC(ARGS);
@@ -39,41 +39,38 @@ static
     void *tmpres = mth->func(mth->args);
     mth->code = (signed char *)tmpres;
     mth->result = tmpres + 1;
-    return (void *)mth->code;
+#ifndef _WIN32
+    return *mth->code;
+#else
+    return (void*)mth->code;
+#endif
 }
 
 int parallel_run(MyThread *thrds[], unsigned char fcount)
 #ifndef _WIN32
 {
     for (int i = 0; i < fcount; ++i)
-    {
 #ifndef NO_PARALLEL
-        thrd_create(&thrds[i]->id, RunInNewThrd, (void *)thrds[i]);
+        thrd_create(&thrds[i]->id, RunInNewThrd, (void*)thrds[i]);
 #else
-        thrds[i]->code = thrd_create_dummy(
-            &thrds[i]->id, RunInNewThrd, (void *)thrds[i]);
+        thrd_create_dummy(
+            &thrds[i]->id, RunInNewThrd, (void*)thrds[i]);
 #endif
-    }
 
-    for (int i = 0; i < fcount; i++)
-    {
 #ifndef NO_PARALLEL
-        thrd_join(thrds[i]->id, &thrds[i]->code);
+    for (int i = 0; i < fcount; i++)
+        thrd_join(thrds[i]->id, NULL);
 #endif
-        printf("Thread%03lu exited with %d\n",
-               thrds[i]->id, thrds[i]->code);
-    }
+    return 0;
 }
 #else
 {
     for (int i = 0; i < fcount; ++i)
-    {
 #ifndef NO_PARALLEL
-        pthread_create(&thrds[i]->id, NULL, RunInNewThrd, (void *)(thrds[i]));
+        pthread_create(&thrds[i]->id, NULL, RunInNewThrd, (void*)(thrds[i]));
 #else
-        thrd_create_dummy(&thrds[i]->id, RunInNewThrd, (void *)thrds[i]);
+        thrd_create_dummy(&thrds[i]->id, RunInNewThrd, (void*)thrds[i]);
 #endif
-    }
 #ifndef NO_PARALLEL
     for (int i = 0; i < fcount; i++)
         pthread_join(thrds[i]->id, NULL);
