1088e6b01d3cd50c639bb27676eb6956d884b3cf
KennethBihag <1618kbb@gmail.com>
2024-04-13 02:50:29 +0800

    Modify tests for profiler

--- profiler/src/main.cpp
+++ profiler/src/main.cpp
@@ -15,6 +15,7 @@ using namespace std;
 
 int g_argc;
 const char **g_argv;
+const char **g_argv2;
 
 void pfBubblesort()
 {
@@ -23,7 +24,7 @@ void pfBubblesort()
 
 void pfMergesort()
 {
-    mergesort_test(g_argc, g_argv);
+    mergesort_test(g_argc, g_argv2);
 }
 
 void pfPermute2()
@@ -40,19 +41,19 @@ void pfPermute4()
     permute_test((void*)permuteArgs);
 }
 
-void pfPermute8()
-{
-    char permuteArgs[] = { 8,0,0,0,
-        42, 87, 15, 61, 33,
-        94, 20, 76 };
-    permute_test((void*)permuteArgs);
-}
-
-void pfPermute10()
+void pfPermute10_1()
 {
     char permuteArgs[] = { 10,0,0,0,
         87, 33, 0, 1, 3,
         94, 40, 76, 69, 13};
+    permute_test((void*)permuteArgs);
+}
+
+void pfPermute10_2()
+{
+    char permuteArgs[] = { 10,0,0,0,
+        30, 22, 0, 58, 3,
+        7, 39, 76, 96, 13};
     permute_test((void*)permuteArgs);
 }
 
@@ -61,16 +62,24 @@ int main(int argc, const char *argv[])
     cout << "Profiling..." << endl;
 
     g_argc = 19;
+
     const char *nums[] = {
         "", "20", "-4", "7", "0", "1", "45", "-33", "-6", "99",
         "10", "-3", "77", "69", "13", "24", "-52", "-6", "99"};
     g_argv = nums;
 
+    const char *nums2[] = {
+        "", "20", "-4", "7", "0", "1", "45", "-33", "-6", "99",
+        "10", "-3", "77", "69", "13", "24", "-52", "-6", "99"};
+    g_argv2 = nums2;
+
     vector<Profiler*> profs;
 
     string fname("bubblesort_test");
     profs.push_back(new Timfiler(pfBubblesort, p_unit::ms, fname));
-    profs.push_back(new Memfiler(pfBubblesort, p_unit::kb, fname));
+
+    fname = "mergesort_test";
+    profs.push_back(new Memfiler(pfMergesort, p_unit::kb, fname));
 
     fname = "permute_test2";
     profs.push_back(new Timfiler(pfPermute2,p_unit::ms, fname));
@@ -78,11 +87,11 @@ int main(int argc, const char *argv[])
     fname = "permute_test4";
     profs.push_back(new Timfiler(pfPermute4,p_unit::ms, fname));
 
-    fname = "permute_test8";
-    profs.push_back(new Timfiler(pfPermute8,p_unit::ms, fname));
+    fname = "permute_test10_1";
+    profs.push_back(new Timfiler(pfPermute10_1,p_unit::ms, fname));
 
-    fname = "permute_test10";
-    profs.push_back(new Timfiler(pfPermute10,p_unit::ms, fname));
+    fname = "permute_test10_2";
+    profs.push_back(new Timfiler(pfPermute10_2,p_unit::ms, fname));
 
     Parallelfiler prfl(profs);
     prfl.ParallelRun();
