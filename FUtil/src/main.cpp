00e598d205c50c5cc517377ded98920b1f0a9044
KennethBihag <1618kbb@gmail.com>
2025-05-18 21:57:11 +0800

    Refactor FUtil main

--- FUtil/src/main.cpp
+++ FUtil/src/main.cpp
@@ -12,99 +12,117 @@
 using namespace std;
 
 void *CpyThread(void *arg);
-struct CpyThrdArg{
+struct CpyThrdArg
+{
     string inpath;
     string outpath;
     long long offset;
     long long bytes;
 };
 
-int main(int argc, const char *argv[]){
-    if(argc<4){
+int main(int argc, const char *argv[])
+{
+    if (argc < 4)
+    {
         cout << "USAGE: " << argv[0]
-             << " <<split|merge> <file path> [parts]"
-             << " | crypt <file path> <password>>"
+             << " << 0(split) | 1(merge) > <file path> [parts]"
+             << " | 2(crypt) <file path> <password>>"
              << endl;
         return EXIT_FAILURE;
     }
 
-    string comm(argv[1]);
+    int comm(stoi(argv[1]));
     string inpath(argv[2]);
     long long parts = 0;
     size_t t = inpath.find_last_of("\\/");
-    string leaf = inpath.substr(t+1);
+    string leaf = inpath.substr(t + 1);
     string indir(inpath);
-    indir.erase(indir.length()-leaf.length());
+    indir.erase(indir.length() - leaf.length());
 
-    if(comm.compare("split")==0 || comm.compare("merge")==0){
+    if (comm < 2)
+    {
         parts = atoll(argv[3]);
-        if(parts < 2)
+        if (parts < 2)
             return EXIT_FAILURE;
     }
 
-    if(comm.compare("split")==0){
+    switch (comm)
+    {
+    case 0:
+    {
         long long fileSz = GetFileSize(inpath);
-        if(fileSz < 0)
+        if (fileSz < 0)
             return EXIT_FAILURE;
-        long long chunkSz = fileSz/parts;
-        if(chunkSz < 1)
+        long long chunkSz = fileSz / parts;
+        if (chunkSz < 1)
             return EXIT_FAILURE;
-        
+
         pthread_t *ts = new pthread_t[parts];
         CpyThrdArg *args = new CpyThrdArg[parts];
-        for(int id=0; id<parts; id++){
+        for (int id = 0; id < parts; id++)
+        {
             char prefix[4];
             sprintf(prefix, "%hhd", id);
             string outpath(indir);
             outpath.append(prefix).append("_").append(leaf);
-            long long offSet = id*chunkSz;
-            if(id==parts-1){
-                chunkSz = fileSz - id*chunkSz;
+            long long offSet = id * chunkSz;
+            if (id == parts - 1)
+            {
+                chunkSz = fileSz - id * chunkSz;
             }
             args[id] = CpyThrdArg{inpath, outpath, offSet, chunkSz};
         }
 
-        for(int i=0; i<parts; i++){
-            pthread_create(ts+i, NULL, CpyThread, args+i);
+        for (int i = 0; i < parts; i++)
+        {
+            pthread_create(ts + i, NULL, CpyThread, args + i);
         }
 
-        for(int i=0; i<parts; i++){
+        for (int i = 0; i < parts; i++)
+        {
             pthread_join(ts[i], NULL);
         }
 
-        delete [] ts;
-
-    } else if(comm.compare("merge")==0){
+        delete[] ts;
+    }
+    break;
+    case 1:
+    {
         string outpath(indir);
         outpath.append("merged").append("_").append(leaf);
-        for(int id=0; id<parts; id++){
+        for (int id = 0; id < parts; id++)
+        {
             char prefix[4];
-            sprintf(prefix,"%hhd",id);
+            sprintf(prefix, "%hhd", id);
             string part(indir);
             part.append(prefix).append("_").append(leaf);
             auto pfsz = GetFileSize(part);
             CpyFBytesToF(part, outpath, 0, pfsz, 1);
         }
-    } else if(comm.compare("crypt")==0){
-
+    }
+    break;
+    case 2:
+    {
         Cryptographer c(inpath.c_str(), argv[3]);
         string outpath(inpath);
         outpath.append(".ken");
-        ofstream ofs(outpath,ios::binary|ios::out);
-        Printer p(c,ofs);
+        ofstream ofs(outpath, ios::binary | ios::out);
+        Printer p(c, ofs);
         c.GetOrigBytes();
         c.ToggleEncryption();
         p.Print();
         ofs.close();
-    } else {
+    }
+    break;
+    default:
         cout << "NOT IMPLEMENTED" << endl;
     }
-
     return EXIT_SUCCESS;
 }
 
-void *CpyThread(void *arg){
-    CpyThrdArg &args = (*(CpyThrdArg*)arg);
+void *CpyThread(void *arg)
+{
+    CpyThrdArg &args = (*(CpyThrdArg *)arg);
     CpyFBytesToF(args.inpath, args.outpath, args.offset, args.bytes);
     return 0;
 }
