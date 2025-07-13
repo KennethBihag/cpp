e10d1f666466f89303d6a832938a292368d843a3
KennethBihag <1618kbb@gmail.com>
2025-07-13 15:39:14 +0800

    Modify Server::Activate to use select function
    Only works well on TCP for now

--- network/include/connection.h
+++ network/include/connection.h
@@ -17,14 +17,15 @@ class IConnection {
 protected:
     std::unique_ptr<CSocket> m_pISocket;
     std::unique_ptr<SocketFactory> m_pSF;
+    TIMEVAL m_tvTimeout;
 
     void Close();
 public:
     IConnection();
     IConnection(const std::string&, const std::string&, int, int, int);
     virtual ~IConnection();
-    virtual int Send(const char (&)[g_rcvLim], size_t) = 0;
-    virtual int Receive(char (&)[g_rcvLim]) = 0;
+    virtual int Send(const char (&)[g_rcvLim], size_t) const = 0;
+    virtual int Receive(char (&)[g_rcvLim]) const = 0;
     const std::string GetSockInfoStr() const;
     const std::string GetAddrInfoStr() const;
     void Activate(decltype(bind)) const;
@@ -39,20 +40,26 @@ public:
       int = 0);
     Client(CSocket*);
     void Activate() const override;
-    int Send(const char (&)[g_rcvLim], size_t) override final;
-    int Receive(char (&)[g_rcvLim]) override final;
+    int Send(const char (&)[g_rcvLim], size_t) const override final;
+    int Receive(char (&)[g_rcvLim]) const override final;
     ~Client();
 };
 
 class Server: public IConnection{
-    std::vector<std::unique_ptr<Client>> m_vupPeers;
+    mutable std::vector<std::unique_ptr<Client>> m_vupPeers;
+    fd_set m_fdsMaster;
 public:
     Server(const std::string& = "", const std::string& = "",
       int = AF_UNSPEC, int = 0);
     void Activate() const override;
-    void Accept();
-    int Send(const char (&)[g_rcvLim], size_t) override final;
-    int Receive(char (&)[g_rcvLim]) override final;
+    void Accept() const;
+    int Send(const char (&)[g_rcvLim], size_t) const override final;
+    int Receive(char (&)[g_rcvLim]) const override final;
+private:
+    void LogClientCount() const;
+    std::vector<std::unique_ptr<Client>>::iterator GetClById(SOCKET) const;
+    int Send(const char (&)[g_rcvLim], size_t, SOCKET) const;
+    int Receive(char (&)[g_rcvLim], SOCKET) const;
 };
 
 }
--- network/lib/connection.cpp
+++ network/lib/connection.cpp
@@ -10,7 +10,7 @@ namespace Knt
 {
 
 // IConnection
-IConnection::IConnection() : m_pISocket(nullptr), m_pSF(nullptr)
+IConnection::IConnection() : m_pISocket(nullptr), m_pSF(nullptr), m_tvTimeout{0}
 {
     ;
 }
@@ -83,7 +83,7 @@ void Client::Activate() const {
     IConnection::Activate(connect);
 }
 
-int Client::Send(const char (&buf)[g_rcvLim], size_t sendLen) {
+int Client::Send(const char (&buf)[g_rcvLim], size_t sendLen) const {
     if(m_pISocket->m_state != CSocket::State::ON)
         return -1;
     std::cout << "Try sending...\n";
@@ -99,7 +99,7 @@ int Client::Send(const char (&buf)[g_rcvLim], size_t sendLen) {
         return sent;
     }
 }
-int Client::Receive(char (&buf)[g_rcvLim]) {
+int Client::Receive(char (&buf)[g_rcvLim]) const {
     if(m_pISocket->m_state != CSocket::State::ON)
         return -1;
     std::cout << "Try receiving...\n";
@@ -122,7 +122,7 @@ Server::Server(const string& host, const string& port,
   int family, int sockType) : IConnection(host, port, family, sockType,
   AI_PASSIVE)
 {
-    ;
+    FD_ZERO(&m_fdsMaster);
 }
 
 void Server::Activate() const {
@@ -131,9 +131,58 @@ void Server::Activate() const {
         if(listen(m_pISocket->m_sSock, 10))
             ThrowNetworkError("Could not listen!");
     }
+    Logger::Log(Logger::Out, "Bound and Listening", GetSockInfoStr());
+
+    FD_SET(m_pISocket->m_sSock, &m_fdsMaster);
+    SOCKET maxSocket = m_pISocket->m_sSock;
+
+    while(true){
+        fd_set fdsRead = m_fdsMaster;
+        fd_set fdsWrite = m_fdsMaster;
+        if (select(maxSocket+1, &fdsRead, &fdsWrite, 0, &m_tvTimeout) < 0)
+            ThrowNetworkError("select failed");
+        for(SOCKET i=1; i<=maxSocket; ++i){
+            if(FD_ISSET(i, &fdsRead)){
+                if(i == m_pISocket->m_sSock){ // new connection
+                    Accept();
+                    const SOCKET &clSck = m_vupPeers.back()->m_pISocket->m_sSock;
+                    FD_SET(clSck, &m_fdsMaster);
+                    maxSocket = std::max(clSck, maxSocket);
+                } else { // got message from a client
+                    char rcvMsg[g_rcvLim]{};
+                    auto itr = GetClById(i);
+                    if(Receive(rcvMsg, i) >= 0){
+                        char buff[256]{};
+                        sprintf(buff, "Received from sock[%d]",(*itr)->m_pISocket->m_sSock);
+                        Logger::Log(Logger::Out, buff, rcvMsg);
+                    } else {
+                        m_vupPeers.erase(itr);
+                        FD_CLR(i, &m_fdsMaster);
+                    }
+                    
+                }
+                LogClientCount();
+                break;
+            }
+            if(FD_ISSET(i, &fdsWrite)){
+                char buff[g_rcvLim]{};
+                static const char *reply = "Hello from server non-blocking!";
+                sprintf(buff, "%s", reply);
+                int sent = Send(buff, strlen(reply), i);
+                // close connection after sending
+                auto itr = GetClById(i);
+                sprintf(buff, "Sent reply to [%d]", (*itr)->m_pISocket->m_sSock);
+                Logger::Log(Logger::Inf, buff, "");
+                m_vupPeers.erase(itr);
+                FD_CLR(i, &m_fdsMaster);
+                LogClientCount();
+                break;
+            }
+        }
+    }
 }
 
-void Server::Accept(){
+void Server::Accept() const{
     if(m_pISocket->m_sockType != SOCK_DGRAM){
         int len = m_pISocket->m_sALen;
         sockaddr_storage storage{};
@@ -150,7 +199,7 @@ void Server::Accept(){
     }
 }
 
-int Server::Send(const char (&msg)[g_rcvLim], size_t sendLen) {
+int Server::Send(const char (&msg)[g_rcvLim], size_t sendLen) const {
     int sent = -1;
     if(m_pISocket->m_sockType != SOCK_DGRAM){
         sent = m_vupPeers.back()->Send(msg, sendLen);
@@ -172,7 +221,33 @@ int Server::Send(const char (&msg)[g_rcvLim], size_t sendLen) {
     }
     return sent;
 }
-int Server::Receive(char (&msg)[g_rcvLim]) {
+
+int Server::Send(const char (&msg)[g_rcvLim], size_t sendLen, SOCKET s) const {
+    auto clIt = GetClById(s);
+    Client &cl = **clIt;
+
+    int sent = -1;
+    if(m_pISocket->m_sockType != SOCK_DGRAM){
+        sent = cl.Send(msg, sendLen);
+    } else {
+/*         sockaddr_storage storage{};
+        int len = m_pISocket->m_sALen;
+        sent = sendto(m_pISocket->m_sSock, msg, sendLen, 0, (sockaddr*)&storage, len); */
+
+        auto &clientPeer = *m_vupPeers.back();
+        sockaddr *pAddr = (sockaddr*)&(clientPeer.m_pISocket->m_sockAddr);
+        int len = clientPeer.m_pISocket->m_sALen;
+        sent = sendto(m_pISocket->m_sSock, msg, sendLen, 0, pAddr, len);
+        if(sent < 0)
+            ThrowNetworkError("sendto failed");
+        clientPeer.m_pISocket->UpdateState();
+        m_vupPeers.pop_back();
+
+    }
+    return sent;
+}
+
+int Server::Receive(char (&msg)[g_rcvLim]) const {
     if(m_pISocket->m_sockType != SOCK_DGRAM)
         return m_vupPeers.back()->Receive(msg);
     else{
@@ -188,6 +263,36 @@ int Server::Receive(char (&msg)[g_rcvLim]) {
         return received;
     }
 
+}
+
+int Server::Receive(char (&msg)[g_rcvLim], SOCKET s) const {
+    Client &cl = **GetClById(s);
+    if(m_pISocket->m_sockType != SOCK_DGRAM)
+        return cl.Receive(msg);
+    else{
+        sockaddr_storage storage{};
+        int len = m_pISocket->m_sALen;
+        int received = recvfrom(m_pISocket->m_sSock, msg, g_rcvLim-1, 0,
+          (sockaddr*)&storage, &len);
+        CSocket *pClientSock = new CSocket(0, (sockaddr*)&storage, len, m_pISocket->m_sockType, CSocket::State::ON);
+        m_vupPeers.emplace_back(make_unique<Client>(pClientSock));
+        string peerInfo = Interfaces::GetSockInfo((sockaddr*)&storage, len);
+        Logger::Log(Logger::Inf, "Received from info", peerInfo);
+
+        return received;
+    }
+}
+
+std::vector<std::unique_ptr<Client>>::iterator Server::GetClById(SOCKET s) const {
+    for(auto i=m_vupPeers.begin(); i<m_vupPeers.end(); ++i)
+        if(s == (*i)->m_pISocket->m_sSock) return i;
+    return m_vupPeers.end();
+}
+
+void Server::LogClientCount() const {
+    char buff[128];
+    sprintf(buff, "%d clients", m_vupPeers.size());
+    Logger::Log(Logger::Inf, __FUNCTION__, buff);
 }
 
 } // namespace Knt
