a86a3dbb205b43ce37439cd9ced825e7e4ad7936
KennethBihag <1618kbb@gmail.com>
2025-12-08 19:14:07 +0800

    Use boost::test dll & unuse precompiled header

--- common/include/pch.hpp
+++ common/include/pch.hpp
@@ -1,6 +1,12 @@
 #include <iostream>
 #include <vector>
 
+#ifdef USE_BOOST_DLL
+#define BOOST_TEST_DYN_LINK
+#include <boost/test/unit_test.hpp>
+#else
 #include <boost/test/included/unit_test.hpp>
+#endif
+
 #include <boost/test/data/test_case.hpp>
 #include <boost/test/data/monomorphic.hpp>
